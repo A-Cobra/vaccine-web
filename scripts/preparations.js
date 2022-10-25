45e99653a710046d79ff50d6a1a32d80dac87fb6
Cobra <conradbravina@gmail.com>
2022-10-25 13:10:27 -0500

    Finally creted the functions and classses to be a ble to interact and display pop-up content
    We used JavaScipt and CSS and were able to have a functional pop up content, it limits to 1 information opened or popped-up per time, it also lets you close the current one

--- scripts/preparations.js
+++ scripts/preparations.js
@@ -1,14 +1,47 @@
 // alert("Hi there little friends");
 
+
+
+
+const checkActive = ()=>{
+    //Checks if there is an acive head-pop container
+    if (popContainer.classList[2] != "null" && popContainer.classList.contains("active")){
+        //It means there is a class active identifier
+        // and the clas active
+        console.log("One is active");
+        return true;
+    }
+    else{
+        console.log("There are no active ones")
+        return false;
+    }
+}
+
+const checkItself = ()=>{
+    //Checks if the active pop-head 
+    //is the one you clicked again
+    if (clickedDiv.classList[2] + "a" === popContainer.classList[2]){
+        return true;
+    }
+    return false;
+     
+}
+
+
 const popContainer = document.querySelector("#pop-container");
 // console.log(popContainer);
 const deployArray = document.querySelectorAll(".pop-head");
 // console.log(deployArray);
 // console.log(deployArray[1]);
 // console.log(deployArray[0].classList[2]);
-popContainer.classList.add(deployArray[0].classList[2]);
-let previousSelected = "_1";
-popContainer.classList.remove(previousSelected);
+// popContainer.classList.add(deployArray[0].classList[2]);
+let previousSelected; //Will contain the div that was selected previously
+// previousSelected = document.querySelector("._3");
+console.log("previousSelected");
+console.log(previousSelected);
+// popContainer.classList.remove(previousSelected);
+
+let clickedDiv;
 // console.log("=====");
 // console.log("=====");
 // console.log("=====");
@@ -20,9 +53,10 @@ if(deployArray){
     console.log("a");
     deployArray.forEach(content=>{
         content.addEventListener("click", (event)=>{
+                // It all starts with a click
+
                 //Gives You the path
                 //And then the div itself
-                let clickedDiv;
 
                 //IF the click was detected correctly
 
@@ -60,10 +94,66 @@ if(deployArray){
                 //We can proceed to change classes 
                 //and "pop up" the content
 
+                // console.log(checkItself());
+                if (clickedDiv /*is defined*/){
+                    //Check if there is another one active
+                    //If so
+                    let oneActive = checkActive();
+                    if(oneActive){
+                        //We check if it is the same container
+                        //To "close it"
+                        let itself = checkItself();
+                        if(itself){
+                            //WE CHANGE THE ACTIVE CLASS INTO NON-ACTIVE
+                            clickedDiv.classList.replace(clickedDiv.classList[1], "non-active");
+                            //WE REPLACE ALSO THE MIN CONTAINER INFORMATION 
+                            popContainer.classList.replace(popContainer.classList[1], "non-active");
+                            popContainer.classList.replace(popContainer.classList[2], "null");
+                        }
+
+                        //Else, we need to pop the new one
+                        //and also we need to change the previous one
+                        else{
+                            //FIRST WE CLOSE THE ONE THAT WAS OPENED
+                            //BEFORE
+                            
+                            previousSelected.classList.replace(previousSelected.classList[1], "non-active");
+                            //THEN, WE "ACTIVATE" THE NEW CLASS
+                            clickedDiv.classList.replace(clickedDiv.classList[1], "active");
+                            //THEN, WE CHANGE THE POP CONTAINERS CLASS 
+                            popContainer.classList.replace(popContainer.classList[2], clickedDiv.classList[2] + "a");
+                            //FINALLY, WE CHANGE THE PREVIOUSSELECTED TO 
+                            //THE CURRENT ONE
+                            previousSelected = clickedDiv;
+                        }
+                    }
+
+                    //Else, there are no active ones and we just
+                    //"Open the pop-head" and add some classes
+                    else{
+                        //We set the previousSelected for future purposes
+                        previousSelected = clickedDiv;
+                        //We set the main container's Class
+                        popContainer.classList.replace(popContainer.classList[2], clickedDiv.classList[2] + "a");
+                        //we also set it to active
+                        popContainer.classList.replace(popContainer.classList[1], "active");
+                        //Now we set the clickedDiv's active class
+                        clickedDiv.classList.replace(clickedDiv.classList[1], "active");
+                    }
+                    
+                    //FOR ADDING THE NEW CLASS:
+                    //popContainer.classList.replace(popContainer.classList[2], "new-class");
+
+                }
+
                 console.log(clickedDiv);
+                console.log("--");
+                console.log("--");
+                console.log("--");
+                checkActive();
                 // console.log(event.path);
-            }
-        );
+            
+        });
 
     });
 }
